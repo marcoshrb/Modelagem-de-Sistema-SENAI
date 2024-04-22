c80692d018c9c45d3e267108e00c4075507b1675
guilherme <grtppg@gmail.com>
2024-04-22 10:59:21 -0300

    pushzin

---
+++ src/components/Nav-bar/Nav-bar.jsx
@@ -0,0 +1,13 @@
+import style from "./Nav-bar.module.css"
+import Logo from "../../assets/hippo.png"
+import { Outlet, useNavigate } from "react-router-dom";
+
+export const CardLogin = () => {
+    return (
+        <>
+            <nav className={style["Nav"]}>
+
+            </nav>
+        </>
+    );
+}
