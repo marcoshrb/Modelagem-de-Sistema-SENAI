1342e27aca103aef2defec4b1580119b3deddfe6
guilherme <grtppg@gmail.com>
2024-04-23 08:52:58 -0300

    navbar

--- src/components/Nav-bar/Nav-bar.jsx
+++ src/components/Nav-bar/Nav-bar.jsx
@@ -1,13 +1,29 @@
-import style from "./Nav-bar.module.css"
-import Logo from "../../assets/hippo.png"
+import style from "./Nav-bar.module.css";
+import Logo from "../../assets/hippo.png";
 import { Outlet, useNavigate } from "react-router-dom";
 
 export const CardLogin = () => {
     return (
         <>
-            <nav className={style["Nav"]}>
-
-            </nav>
+            <div className={style.container}>
+                <div className={style.phone}>
+                    <div className={style.content}>
+                        <nav role="navigation">
+                            <div className={style.menuToggle}>
+                                <input type="checkbox" />
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <ul className={style.menu}>
+                                    <li><a href="#">Jogos</a></li>
+                                    <li><a href="#">Prêmios</a></li>
+                                    <li><a href="#">Perfil</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
